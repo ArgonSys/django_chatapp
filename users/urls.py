b6445e716b772844858a79bf3cad71bd313615bb
argonsys <kenon12552@icloud.com>
2023-12-23 15:45:44 +0900

    users app_name追加

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from .views import SignupView, LoginView, LogoutView
 
 
+app_name = "users"
+
 urlpatterns = [
     path("signup/", SignupView.as_view(), name="signup"),
     path("login/", LoginView.as_view(), name="login"),
