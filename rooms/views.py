5c88309ae35f51528ec603468c6c5e853a87c620
argonsys <kenon12552@icloud.com>
2023-12-23 21:08:47 +0900

    RoomCreateViewの作成

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .models import Room
+from .forms import RoomForm
+
+
+class RoomCreateView(LoginRequiredMixin, CreateView):
+    login_url = reverse_lazy("users:login")
+    redirect_field_name = "redirect_to"
+
+    model = Room
+    template_name = "rooms/new.html"
+    form_class = RoomForm
