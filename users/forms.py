b13eb884f5c068b11851cfa77f3cf4343c701339
argonsys <kenon12552@icloud.com>
2023-12-23 14:43:09 +0900

    Authenticationフォームの追加

--- users/forms.py
+++ users/forms.py
@@ -1,10 +1,14 @@
 from django.contrib.auth.forms import (
+    AuthenticationForm as BaseAuthenticationForm,
     UserCreationForm as BaseUserCreationForm,
     UserChangeForm as BaseUserChangeForm
 )
 
 from .models import User
 
+
+class AuthenticationForm(BaseAuthenticationForm):
+    pass
 
 class UserCreationForm(BaseUserCreationForm):
     class Meta(BaseUserCreationForm.Meta):
