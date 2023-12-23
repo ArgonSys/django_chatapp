35d09d0963676bcbf0ee3741183a046a6cac3860
argonsys <kenon12552@icloud.com>
2023-12-23 19:25:25 +0900

    UserUpdateVIewへのルーティング

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 
-from .views import SignupView, LoginView, LogoutView, PasswordResetView
+from .views import(
+    SignupView, LoginView, LogoutView,
+    PasswordResetView, UserUpdateView
+)
 
 
 app_name = "users"
@@ -10,4 +13,5 @@ urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
     path("password/", PasswordResetView.as_view(), name="password_reset"),
+    path("<int:pk>/edit", UserUpdateView.as_view(), name="edit")
 ]
