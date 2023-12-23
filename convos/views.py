cf881af7e20b7b826ca3e07062fc0326915cac4f
argonsys <kenon12552@icloud.com>
2023-12-23 11:43:41 +0900

    ConvosViewクラス作成

--- convos/views.py
+++ convos/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import View
+from django.views.generic import CreateView
 
-# Create your views here.
+from django.http import HttpResponse
+
+
+class ConvosView(View):
+    def get(self, request, *args, **kwargs):
+        return HttpResponse(b"ConvosView")
