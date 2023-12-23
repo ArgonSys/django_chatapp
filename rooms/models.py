41215c0f6f988801bc716c4b7b262d32dcebd10e
argonsys <kenon12552@icloud.com>
2023-12-23 20:45:12 +0900

    Roomモデル作成

--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class Room(models.Model):
+    name = models.CharField(_(""), max_length=50)
+    users = models.ManyToManyField("users.User", verbose_name=_("members"))
+
+    class Meta:
+        verbose_name = _("room")
+        verbose_name_plural = _("rooms")
+        db_table = "rooms"
