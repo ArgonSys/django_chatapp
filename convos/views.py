373db2cd929143392e48459f7dfcc437cde6cef6
argonsys <kenon12552@icloud.com>
2023-12-23 15:54:01 +0900

    ConvosViewにLoginRequiredMixin

--- convos/views.py
+++ convos/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from django.http import HttpResponse
 
 
-class ConvosView(View):
+
+class ConvosView(LoginRequiredMixin, View):
+    login_url = "/users/login/"
+    redirect_field_name = "redirect_to"
+
     def get(self, request, *args, **kwargs):
         return render(request, "convos/index.html")
