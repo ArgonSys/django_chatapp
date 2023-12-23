92c559ca4a970bac4eee498ad50dc16d4de08b2c
argonsys <kenon12552@icloud.com>
2023-12-23 14:43:35 +0900

    Signup, Login, Logoutビュークラスの作成

--- users/views.py
+++ users/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.views.generic import CreateView
+from django.contrib.auth.views import(
+    LoginView as BaseLoginView,
+    LogoutView as BaseLogoutView
+)
 
-# Create your views here.
+
+from .forms import UserCreationForm, UserChangeForm, AuthenticationForm
+
+
+class SignupView(CreateView):
+    template_name = "users/signup.html"
+    form_class = UserCreationForm
+    success_url = "/"
+
+class LoginView(BaseLoginView):
+    template_name = "users/login.html"
+    form_class = AuthenticationForm
+    success_url = "/"
+
+class LogoutView(BaseLogoutView):
+    pass
