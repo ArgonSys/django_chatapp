c1273593000b146caaf8a386c72deac4bdb7f0e6
argonsys <kenon12552@icloud.com>
2023-12-24 21:55:24 +0900

    usersカラムの定義を改行

--- rooms/models.py
+++ rooms/models.py
@@ -2,8 +2,13 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 class Room(models.Model):
-    name = models.CharField(_(""), max_length=50)
-    users = models.ManyToManyField("users.User", verbose_name=_("members"))
+    name = models.CharField(_("name"), max_length=50)
+    users = models.ManyToManyField(
+        "users.User",
+        verbose_name=_("members"),
+        related_name="rooms",
+        related_query_name="room"
+    )
 
     class Meta:
         verbose_name = _("room")
